f4ed9d2e73658787651eb0d5603f7d31ff6edb52
octaviohd <ohd.mail@gmail.com>
2015-08-04 21:45:14 +0200

    Initial Commit

---
+++ SSent/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SSent
+//
+//  Created by Octavio Hernandez Diaz on 04/08/15.
+//  Copyright (c) 2015 Trading. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
